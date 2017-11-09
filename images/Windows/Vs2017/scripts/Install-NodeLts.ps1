57faf57a7df9c2df4f49a1088ed21eba389e87d7
Lokesh Gopu <logopu@microsoft.com>
2017-11-09 10:52:11 -0500

    Fixed cache, prefix path problem

--- images/Windows/Vs2017/scripts/Install-NodeLts.ps1
+++ images/Windows/Vs2017/scripts/Install-NodeLts.ps1
@@ -9,24 +9,25 @@
 
 Import-Module -Name ImageHelpers -Force
 
+$PrefixPath = 'C:\npm\prefix'
+$CachePath = 'C:\npm\cache'
+
+New-Item -Path $PrefixPath -Force -ItemType Directory
+New-Item -Path $CachePath -Force -ItemType Directory
+
 choco install nodejs-lts --version 6.11.5 -y --force
 
-New-Item -Path C:\npm\prefix -Force -ItemType Directory
-New-Item -Path C:\npm\cache -Force -ItemType Directory
-
-setx NPM_CONFIG_PREFIX=C:\npm\prefix /M
-setx NPM_CONFIG_CACHE=C:\npm\cache /M
-
-$env:NPM_CONFIG_PREFIX=C:\npm\prefix
-$env:NPM_CONFIG_CACHE=C:\npm\cache
-
+Add-MachinePathItem $PrefixPath
+Add-MachinePathItem $CachePath
 $env:Path = Get-MachinePath
 
 Write-Host "Node $(node --version) on path"
 Write-Host "Npm $(npm -version) on path"
 
-npm install -g glup-cli
+npm config set cache $PrefixPath
+npm config set prefix $CachePath
+
+npm install -g gulp-cli
 npm install -g grunt-cli
 npm install -g bower
 npm install -g cordova
-npm install -g iconic
