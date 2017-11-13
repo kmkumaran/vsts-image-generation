b71dc84fd3a1e276b80637fdae6bc8b4cf4aaa4c
Lokesh Gopu <logopu@microsoft.com>
2017-11-13 17:34:11 -0500

    Updated Path env to include Git paths

--- images/Windows/Vs2017/scripts/Install-Git.ps1
+++ images/Windows/Vs2017/scripts/Install-Git.ps1
@@ -7,8 +7,11 @@
 
 Import-Module -Name ImageHelpers
 
-choco install git --version 2.15.0 -y --package-parameters= "/GitOnlyOnPath /WindowsTerminal /NoShellIntegration"
+choco install git --version 2.15.0 -y --package-parameters= "/GitAndUnixToolsOnPath /WindowsTerminal /NoShellIntegration"
 
+Add-MachinePathItem "C:\Program Files\Git\mingw64\bin"
+Add-MachinePathItem "C:\Program Files\Git\usr\bin"
+Add-MachinePathItem "C:\Program Files\Git\bin"
 $env:Path = Get-MachinePath
 
 Write-Host "Git $(git --version) on path"
