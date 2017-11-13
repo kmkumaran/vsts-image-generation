2977abe8a664b55b6e4b5201c622424c9f58770a
Lokesh Gopu <logopu@microsoft.com>
2017-11-13 15:58:07 -0500

    Added NDK Path env var

--- images/Windows/Vs2017/scripts/Update-AndroidSDK.ps1
+++ images/Windows/Vs2017/scripts/Update-AndroidSDK.ps1
@@ -26,8 +26,11 @@ Expand-Archive -Path .\android-sdk-licenses.zip -DestinationPath 'C:\Program Fil
 # keep newer versions in descending order
 
 $sdk_root = "C:\Program Files (x86)\Android\android-sdk"
+$ndk_path = "C:\ProgramData\Microsoft\AndroidNDK64\android-ndk-r13b"
 
 setx ANDROID_HOME $sdk_root /M
+setx ANDROID_NDK_HOME $ndk_path /M
+setx ANDROID_NDK_PATH $ndk_path /M
 
 Push-Location -Path $sdk.FullName
 
